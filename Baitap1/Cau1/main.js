5f8f97c2f0e68f687b2d8119aa3a0f4cd5d0f73c
letrangCFI <letrang05ctt1@gmail.com>
2025-12-05 15:39:20 +0700

    Add files via upload

---
+++ Baitap1/Cau1/main.js
@@ -0,0 +1,7 @@
+let a1=15;
+let b1=36;
+let a=a1;
+let b=b1;
+let c=b;
+b=a; a=c;
+alert(`Hai số nguyên bạn đã tự cho là a=${a1} và b=${b1}.\nSau khi hoán đổi (hoán vị) hai số ta có a=${a} và b=${b}.`);
